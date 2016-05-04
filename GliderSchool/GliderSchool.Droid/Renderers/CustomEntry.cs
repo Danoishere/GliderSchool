da5a838f817023989e813586052fa325ad37f28a
Dano <dano.roost@gmail.com>
2016-05-04 16:08:20 +0200

    first custom renderer for entry. Doesn't do anything yet

---
+++ GliderSchool/GliderSchool.Droid/Renderers/CustomEntry.cs
@@ -0,0 +1,20 @@
+using GliderSchool.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace GliderSchool.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
